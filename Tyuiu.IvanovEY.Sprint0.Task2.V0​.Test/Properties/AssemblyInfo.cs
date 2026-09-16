1d7ce85a8ed45b70c3b6127905cac7ddc6cb6c2a
Егор Иванов <egoris09009@gmail.com>
2026-09-17 02:03:59 +0500

    Task2

---
+++ Tyuiu.IvanovEY.Sprint0.Task2.V0​.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanovEY.Sprint0.Task2.V0​.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanovEY.Sprint0.Task2.V0​.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a17009af-3d4a-4ca6-8b91-ef3a40ea7d92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
