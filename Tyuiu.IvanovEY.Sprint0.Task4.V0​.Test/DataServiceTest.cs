c85e10b8aff3eeae01b444460c22d383fb68ce81
Егор Иванов <egoris09009@gmail.com>
2026-09-17 03:42:22 +0500

    Task4

---
+++ Tyuiu.IvanovEY.Sprint0.Task4.V0​.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.IvanovEY.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.IvanovEY.Sprint0.Task4.V0​.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void CheckedDivisionNValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+       
+    }
+}
