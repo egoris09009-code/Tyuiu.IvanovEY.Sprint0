e7b75e9b922e6568577395cddcae4dd31a7cba2e
Егор Иванов <egoris09009@gmail.com>
2026-09-17 02:48:46 +0500

    Task3

---
+++ Tyuiu.IvanovEY.Sprint0.Task3.V0​/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.IvanovEY.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.IvanovEY.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
