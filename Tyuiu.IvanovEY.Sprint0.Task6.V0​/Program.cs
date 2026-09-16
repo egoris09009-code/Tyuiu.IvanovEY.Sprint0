17390a210753975665de716de5f7af9e720d8a83
Егор Иванов <egoris09009@gmail.com>
2026-09-17 04:45:53 +0500

    Task6

---
+++ Tyuiu.IvanovEY.Sprint0.Task6.V0​/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.IvanovEY.Sprint0.Task6.V0.Lib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+
+        Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(arraynums));
+
+        Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
+
+        Console.ReadKey();
+    }
+}
+
